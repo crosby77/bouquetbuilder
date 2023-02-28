e67cb10fc7178c34eb1abeade85d62ffac28e0bd
Crosby <crosbycofod@gmail.com>
2023-02-28 08:24:14 -0500

    brings everything to a good starting point

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-const vase = document.querySelector('.vaseImage');
+function handleDragStart(_e) {
+    this.style.opacity = '0.4';
+    console.log("nice drage");
+  }
+  
+  function handleDragEnd(_e) {
+    this.style.opacity = '1';
+  }
+  
+  let items = document.getElementsByClassName("img-responsive");
+  console.log(items)
 
-vase.addEventListener('dragstart', dragStart);
-
-function dragStart(e) {
-    console.log('drag starts...');
-    e.dataTransfer.setData('text/plain', e.target.id);
-    e.target.classList.add('hide');
- }
-
+  for(let i = 0; i < items.length; i ++){
+    items[i].addEventListener('dragstart', handleDragStart);
+    items[i].addEventListener('dragend', handleDragEnd);
+    
+  };
